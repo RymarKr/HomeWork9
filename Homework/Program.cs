8e94138c0ba058d64b17ba5a4667ed690092114d
Kirill <kirill.rymar@gmail.com>
2022-12-29 20:26:09 +0200

    first commit

---
+++ Homework/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 64: Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. Выполнить с помощью рекурсии.
+N = 5 -> "5, 4, 3, 2, 1"
+N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+*/
+Console.Write("Введите натуральное число: ");
+int NumberN = int.Parse(Console.ReadLine());
+Console.Write($"{NumberN}");
+
+Method();
+
+void Method(int n = 1)
+{
+    NumberN--;
+    if (n > NumberN)
+    {
+        return;
+    }
+
+    Console.Write($", {NumberN}");
+
+    Method(n);
+}
---
+++ Homework2/Program.cs
@@ -0,0 +1,22 @@
+﻿/*Задача 66: Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N. 
+Выполнить с помощью рекурсии.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+Console.Write("Введите M: ");
+int NumberN = int.Parse(Console.ReadLine());
+Console.Write("Введите N: ");
+int i = int.Parse(Console.ReadLine());
+
+Console.WriteLine($"{Method(NumberN)} - Это сумма в промежутке от M до N");
+int Method(int NumberN)
+{
+    if (NumberN > i)
+    {
+        return 0;
+    }
+
+    return NumberN + Method(NumberN+1);
+
+}
